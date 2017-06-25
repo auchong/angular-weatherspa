ae2acacbdbb10b8df8e2c408da375b42ac6f109f
Austin Chong <Austin@Austins-MBP.home>
2017-06-25 02:57:46 -0400

    First commit

---
+++ services.js
@@ -0,0 +1,17 @@
+//SERVICES
+weatherApp.service('cityService', function(){
+
+    this.city = 'New York, NY';
+
+});
+
+weatherApp.service('weatherService', ['$resource', function($resource){
+
+    this.getWeather = function(city, days) {
+        var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}});
+
+        return weatherAPI.get({q: city, cnt: days, appid: '1e91875ded79629d930964ad1b49bcfe'})
+
+    };
+
+}]);
