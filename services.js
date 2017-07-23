7b6339481ecf5be4b17059cc67395fc2f0f8e46d
Austin Chong <Austin@Austins-MBP.home>
2017-07-22 20:27:47 -0400

    Change http server request for api

--- services.js
+++ services.js
@@ -8,7 +8,7 @@ weatherApp.service('cityService', function(){
 weatherApp.service('weatherService', ['$resource', function($resource){
 
     this.getWeather = function(city, days) {
-        var weatherAPI = $resource('https://api.openweathermap.org/data/2.5/forecast/daily', {callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}});
+        var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {callback: 'JSON_CALLBACK'}, {get: {method: 'JSONP'}});
 
         return weatherAPI.get({q: city, cnt: days, appid: '1e91875ded79629d930964ad1b49bcfe'})
 
